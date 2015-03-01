55067d0e21d56b3c801b98202f29edc245a0989e
ashbin <ashbinmathew22@gmail.com>
2015-03-02 02:40:42 +0530

    Added client socket code to sent json object in UpdateStatistics. Also added function for updating the statistics database

--- src/UpdateStatistics.java
+++ src/UpdateStatistics.java
@@ -1,3 +1,5 @@
+import java.io.DataOutputStream;
+import java.net.Socket;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -5,7 +7,6 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
 
-import com.mysql.jdbc.DatabaseMetaData;
 
 public class UpdateStatistics {
 	/*
@@ -23,17 +24,19 @@ public class UpdateStatistics {
 	//for database statistics
 	private final String statisticsDb = "statistics"; 
 	
+	private final String statTable = "stats";
+	
 	/*
 	 * connection function : with database name 'dbName'
 	 */
-	public Connection getConnection(String dbName) throws SQLException {
+	public Connection getConnection(String databaseName) throws SQLException {
 		Connection conn = null;
 		Properties connectionProps = new Properties();
 		connectionProps.put("user", this.userName);
 		connectionProps.put("password", this.password);
 
 		conn = DriverManager.getConnection("jdbc:mysql://"
-				+ this.serverName + ":" + this.portNumber + "/" + this.dbName,
+				+ this.serverName + ":" + this.portNumber + "/" + databaseName,
 				connectionProps);
 
 		return conn;
@@ -46,58 +49,46 @@ public class UpdateStatistics {
 	    Statement stmt = null;
 	    try {
 	        stmt = conn.createStatement();
-	        stmt.executeUpdate(command); // This will throw a SQLException if it fails
+	        stmt.executeUpdate(command); 
 	        return true;
 	    } finally {
 
-	    	// This will run whether we throw an exception or not
 	        if (stmt != null) { stmt.close(); }
 	    }
 	}
 	
 	
 	/*
-	 * print table names
+	 * get table names from database as resultset object
 	 */
 	
-	public void printTables(Connection conn){
+	public ResultSet getTables(Connection conn) throws SQLException{
 		// Get the table names
+		ResultSet rs = null;
 		try {
 			java.sql.DatabaseMetaData md = conn.getMetaData();
-			ResultSet rs = md.getTables(null, null, "%", null);
-			while (rs.next()) {
-//			  System.out.println(rs.getString(3));
-				this.printAttributes(conn, rs.getString(3));
-			}
-	    } catch (SQLException e) {
-			System.out.println("ERROR: Could not create the table");
-			e.printStackTrace();
-			return;
-		}
+			rs = md.getTables(null, null, "%", null);
+			return rs;
+
+	    }finally { 
+	    	//do nothing
+	    }
 		
 	}
 	
 	/*
 	 * print attributes of the given table
 	 */
-	public void printAttributes(Connection conn, String tableName){
+	public ResultSet getAttributes(Connection conn, String tableName) throws SQLException{
 		ResultSet rsColumns = null;
-		String typeName = "";
-		String columnNameString = "";
-		long cardinality = 0;
+
 	    java.sql.DatabaseMetaData meta;
 		try {
 			meta = conn.getMetaData();
 			rsColumns = meta.getColumns(null, null, tableName , null);
-			while (rsColumns.next()) {
-				typeName = rsColumns.getString("TYPE_NAME");
-				columnNameString = rsColumns.getString("COLUMN_NAME");
-				cardinality = getCardinality(conn, tableName, columnNameString);
-		      System.out.println(tableName+"   "+ typeName + "   " + columnNameString + "   " + cardinality);
-		    }
-		} catch (SQLException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
+			return rsColumns;
+		} finally {
+			//System.out.println(" attributes of " + tableName);
 		}
 	}
 	
@@ -125,7 +116,95 @@ public class UpdateStatistics {
 		
 		return cardinality;
 	}
+	
+	/*
+	 * update statistics database if any change. 
+	 * original database connection in conn
+	 * statistics database in stat
+	 * statistics table in tableName
+	 */
+	public void statistics(Connection conn, Connection stat , String statTable){
+		
+		// Read table name
+		ResultSet tableSet = null;
+		ResultSet attributeSet = null;
+		ResultSet cardinalitySet = null;
+		String tableName = null;
+		String columnName = null;
+		long cardinality = 0;
+		int statCardinality = 0;
+		Statement stmt = null;
+		String command = null;
+		try {
+			tableSet = getTables(conn);
+			while (tableSet.next()) {
+				tableName = tableSet.getString(3);
+				attributeSet = this.getAttributes(conn, tableName);
+				while (attributeSet.next()) {
+//					typeName = rsColumns.getString("TYPE_NAME");
+					columnName = attributeSet.getString("COLUMN_NAME");
+					cardinality = getCardinality(conn, tableName, columnName);
+					System.out.println(tableName+"   "+  columnName + "   " + cardinality);
+					
+					command = "select count(*) from (select * from " + statTable + " where relation_name = \'" + tableName +
+							"\' and attribute_name = \'"+ columnName + "\')attribute_only ; ";
+					
+					stmt = stat.createStatement();
+					cardinalitySet = stmt.executeQuery(command);
+					cardinalitySet.next();
+					statCardinality = cardinalitySet.getInt(1);
+					
+					if (statCardinality == 0){
+						command = "insert into "+ statTable + "(relation_name,attribute_name,cardinality  ) values (\'" + tableName + "\' ,\'" + 
+								columnName + "\',"+ cardinality+ ")";
+						System.out.println(command);
+						stmt.executeUpdate(command);
+					}
+					else{
+						command = "select * from " + statTable + " where relation_name = \'" + tableName +"\' and attribute_name = \'"+ columnName + "\'";
+						System.out.println(command);
+						ResultSet rs = stmt.executeQuery(command);
+						rs.next();
+						long oldCardinality = rs.getLong(4);
+						int id = rs.getInt(1);
+						if(oldCardinality != cardinality){
+							command = "update "+ statTable+ " set cardinality = "+ cardinality + " where id="+ id ;
+							System.out.println(command);
+//							stmt.executeUpdate(command);
+						}
+					}
+					
+					
+			    }
+				break;
+			}
+			stmt.close();
+			tableSet.close();
+			attributeSet.close();
+			cardinalitySet.close();
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}
+	}
 
+	public String getJsonForRelation(Connection conn){
+		ResultSet rs = null;
+		String relationName = null;
+		try {
+			rs = getTables(conn);
+			rs.next();
+			relationName = rs.getString(3);
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		String str = "{relation : [{name: "+ relationName + "}]}";
+		return str;
+	}
+	
+	
+
+	
 	/*
 	 * function to run these functions
 	 * 
@@ -152,11 +231,22 @@ public class UpdateStatistics {
 			e.printStackTrace();
 			return;
 		}
+		
 
-		this.printTables( conn );
+		//this.statistics(conn, stat, statTable);
+		String str = this.getJsonForRelation(conn);
+		System.out.println(str);
+		try{      
+			Socket s=new Socket("192.168.40.44",9000);  
+			DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
+			dout.writeUTF(str);  
+			dout.flush();  
+			dout.close();  
+			s.close();  
+		}catch(Exception e){System.out.println(e);}  
 		
 		
-		
+		this.statistics(conn, stat, statTable);
 		/*
 		 * close connection
 		 */
@@ -167,8 +257,23 @@ public class UpdateStatistics {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		
 
 	}
+	
+	
+	
+//	class Statistics {
+//		public String relationName = null;
+//		public Statistics(String r) {
+//			// TODO Auto-generated constructor stub
+//			relationName = r;
+//			
+//		}
+//	}
+//	
+	
 	
 }
 
